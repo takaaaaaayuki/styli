f8ec78ecc75c5a2fe05e1bef179bfa1f24fd663a
takaaaaaayuki <s2322045@stu.musashino-u.ac.jp>
2025-05-25 03:22:31 +0900

    Remove Google Cloud credentials block

--- main.py
+++ main.py
@@ -30,11 +30,7 @@ from fastapi import Cookie
 
 load_dotenv()
 
-# ✅ Google Cloud Vision API用のcredentialsをRenderで使うための処理
-if os.getenv("GOOGLE_CREDENTIALS_JSON"):
-    with open("credentials.json", "w") as f:
-        f.write(os.getenv("GOOGLE_CREDENTIALS_JSON"))
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
+
 
 
 UPLOAD_ITEM_DIR = "static/uploads"
